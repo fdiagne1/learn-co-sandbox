b4177ae7b1ccf88366b8e035541133b29c30e1a0
Fatou Diagne <f.m.diagne1@gmail.com>
2020-08-05 20:08:36 +0000

    Automatically backed up by Learn

---
+++ practice.rb
@@ -0,0 +1,27 @@
+def greeting(name)
+  puts "Hi, #{name}!"
+end
+
+def greeting_programmer(name="Ruby programmer", language="Ruby")
+  puts "Hi, #{name}. We heard you are a great #{language} programmer."
+end
+
+evil_monster = "Bowser"
+def princess_peaches_castle
+  puts "#{evil_monster} is trying to kidnap princess peach!"
+end
+
+if 5 > 2
+  print "5 is greater than 2"
+end
+
+case greeting
+end
+
+counter = 0
+loop do
+  counter + 1
+  if counter >= 10
+    break
+  end
+end
